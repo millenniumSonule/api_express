e880e015650778eeee60fe8e1865b60a491022b5
millenniumSonule <anshumansonuley@gmail.com>
2024-08-28 10:37:08 +0530

    .

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
+const users = require('./MOCK_DATA.json');
 
 const PORT = 8000;
 
+//routes
 
+//get api data
+app.get('/api/users', (req, res) => {
+    return res.json(users);
+})
 
-app.listen(PORT, ()=>{
+//get user html
+app.get('/users', (req, res) => {
+    const html = `
+        <ul>
+            ${users.map((user) => `<li> ${user.first_name} </li>`)}
+        </ul>
+    `;
+    res.send(html);
+})
+
+//get rquest with dynamic path parameters
+
+//get user with specific ID
+
+app.get('/users/:id', (req,res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user)
+});
+
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
