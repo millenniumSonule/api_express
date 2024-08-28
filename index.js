3f0b0468614c3f37a513501c55e3fc46f259c602
millenniumSonule <anshumansonuley@gmail.com>
2024-08-28 12:23:41 +0530

    .

--- index.js
+++ index.js
@@ -31,6 +31,30 @@ app.get('/users/:id', (req,res) => {
     return res.json(user)
 });
 
+//create a user // add data
+app.post('/api/users' , (req,res)=> {
+    return res.json({status:'pending'});
+})
+
+
+//handling API for same route for different type of request
+//get, post, put, delete
+//(Grouping of APIs)
+app.route('api/users/:id')
+    .get((req,res)=>{
+        const id = Number(req.params.id);
+        const user = users.find((user) => user.id === id);
+        return res.json(user);
+    })
+    .put((req,res)=>{
+        //edit user with id
+        return  res.json({status:"pending"});
+    })
+    .delete((req,res) => {
+        //delete the user
+        return  res.json({status:"pending"});
+    })
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
