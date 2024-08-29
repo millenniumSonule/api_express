e0ba1e4265d93cc7daec85f95b01d0a9dae7d00a
millenniumSonule <anshumansonuley@gmail.com>
2024-08-29 11:49:26 +0530

    .

--- index.js
+++ index.js
@@ -2,42 +2,20 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 const users = require('./MOCK_DATA.json');
+const { error } = require('console');
 
 const PORT = 8000;
 
-//routes
 
-//get api data
-app.get('/api/users', (req, res) => {
-    return res.json(users);
-})
-
-//get users in html (Server side rendering, good for desktop and 
-//mobile browsers, wont work for mobile apps since this api gives data in html form)
-app.get('/users', (req, res) => {
-    const html = `
-        <ul>
-            ${users.map((user) => `<li> ${user.first_name} </li>`)}
-        </ul>
-    `;
-    res.send(html);
-})
-
-//get rquest with dynamic path parameters
-//get user with specific ID
-app.get('/users/:id', (req,res) => {
-    const id = Number(req.params.id);
-    const user = users.find((user) => user.id === id);
-    return res.json(user)
-});
-
+app.use(express.urlencoded({ extended: false })) // middleware to convert the data from url into form data
 //create a user // add data
-app.post('/api/users' , (req,res)=> {
+app.post('/api/users', (req, res) => {
     const body = req.body;
-    console.log("body ",body)
-    return  res.json({data:'body'});
+    users.push({ ...body, id: users.length + 1 });
+    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data) => {
+        return res.json({ data: body });
+    })
 })
-
 
 
 app.listen(PORT, () => {
