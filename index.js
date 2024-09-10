4395e4ecad24edf000576b28392d5ee8abb04010
millenniumSonule <anshumansonuley@gmail.com>
2024-09-10 12:28:04 +0530

    .

--- index.js
+++ index.js
@@ -2,3 +2,30 @@ const express = require('express');
 const MongoClient = require('mongodb');
 const app = express();
 const PORT = 8000;
+
+// connect to mongoDB
+
+
+// insert data to mongoDB
+
+
+// insert multiple documents
+
+
+// find a document
+
+
+// find multiple documents
+
+// update document
+
+// delete a document
+
+
+
+// create an index
+
+// drop a collection
+
+
+
